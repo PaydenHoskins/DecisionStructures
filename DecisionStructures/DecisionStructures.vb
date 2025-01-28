f710d47e2dceb74f75ceabe74071ef8d893b8576
Payden Hoskins <paydenhoskins@gmail.com>
2025-01-28 08:54:53 -0700

    Do Loops

--- DecisionStructures/DecisionStructures.vb
+++ DecisionStructures/DecisionStructures.vb
@@ -19,68 +19,79 @@ Module DecisionStructures
         'assign 5 to butterscotch
         butterScotch = 5
 
+
         'test Is butterscotch greater than 6
         'display the result on the console
 
-        If butterScotch > 6 Then
-            Console.WriteLine("Butterscotch Is Great! (butterscotch > 6).")
-        End If
+        'If butterScotch > 6 Then
+        '    Console.WriteLine("Butterscotch Is Great! (butterscotch > 6).")
+        'End If
 
-        If butterScotch > 3 Then
-            Console.WriteLine("Butterscotch Is Great! (butterscotch > 3).")
-        End If
+        'If butterScotch > 3 Then
+        '    Console.WriteLine("Butterscotch Is Great! (butterscotch > 3).")
+        'End If
 
-        If butterScotch > 4 Then
-            Console.WriteLine("Butterscotch Is Great! (butterscotch > 4).")
-        End If
+        'If butterScotch > 4 Then
+        '    Console.WriteLine("Butterscotch Is Great! (butterscotch > 4).")
+        'End If
 
-        'If on of the evaluated statments are true it will display and everything else will be skipped
-        If butterScotch > 6 Then
-            Console.WriteLine("Butterscotch Is Great! (butterscotch > 6).")
-        ElseIf butterScotch > 3 Then
-            Console.WriteLine("Butterscotch Is Great! (butterscotch > 3).")
-        ElseIf butterScotch > 4 Then
-            Console.WriteLine("Butterscotch Is Great! (butterscotch > 4).")
-        End If
+        ''If on of the evaluated statments are true it will display and everything else will be skipped
+        'If butterScotch > 6 Then
+        '    Console.WriteLine("Butterscotch Is Great! (butterscotch > 6).")
+        'ElseIf butterScotch > 3 Then
+        '    Console.WriteLine("Butterscotch Is Great! (butterscotch > 3).")
+        'ElseIf butterScotch > 4 Then
+        '    Console.WriteLine("Butterscotch Is Great! (butterscotch > 4).")
+        'End If
 
 
-        butterScotch -= 3
-        'if none of the conditions are met display else
-        If butterScotch > 6 Then
-            Console.WriteLine($"{butterScotch} is a big number!")
-        ElseIf butterScotch > 3 Then
-            Console.WriteLine($"Sorry not tall enough..!")
-        ElseIf butterScotch > 4 Then
-            Console.WriteLine("who gives a ... nevermind.")
-        Else
-            Console.WriteLine($"Not sure what is going on...")
-        End If
+        'butterScotch -= 3
+        ''if none of the conditions are met display else
+        'If butterScotch > 6 Then
+        '    Console.WriteLine($"{butterScotch} is a big number!")
+        'ElseIf butterScotch > 3 Then
+        '    Console.WriteLine($"Sorry not tall enough..!")
+        'ElseIf butterScotch > 4 Then
+        '    Console.WriteLine("who gives a ... nevermind.")
+        'Else
+        '    Console.WriteLine($"Not sure what is going on...")
+        'End If
 
-        Console.WriteLine("Please enter age.")
-        userInput = Console.ReadLine()
 
-        Try
-            butterScotch = CInt(userInput)
-            Select Case butterScotch
-                Case 0 To 3
-                    Console.WriteLine("Too young.")
-                Case 4 To 10
-                    Console.WriteLine("Come back next year kid.")
-                Case 11 To 64
-                    Console.WriteLine("Go right ahead sir!")
-                Case 65 To 99
-                    Console.WriteLine("Are you sure your hips can handle this?")
-                Case > 100
-                    Console.WriteLine("Oh no they escaped the old folks home again!")
-                Case Else
-                    Console.WriteLine("Hey buddy quit messing with the" &
-                                  " machine and put your real age in.")
-            End Select
+        Do
+            Console.WriteLine("Please enter age.")
 
-        Catch ex As Exception
-            Console.WriteLine($"You entered {userInput} which is not" &
-                              " a numeric age so try again... BRO!")
-        End Try
+            userInput = Console.ReadLine()
+
+            Try
+
+                butterScotch = CInt(userInput)
+                Select Case butterScotch
+                    Case 0 To 3
+                        Console.WriteLine("Too young.")
+                    Case 4 To 10
+                        Console.WriteLine("Come back next year kid.")
+                    Case 11 To 64
+                        Console.WriteLine("Go right ahead sir!")
+                    Case 65 To 99
+                        Console.WriteLine("Are you sure your hips can handle this?")
+                    Case > 100
+                        Console.WriteLine("Oh no they escaped the old folks home again!")
+                    Case Else
+                        Console.WriteLine("Hey buddy quit messing with the" &
+                                      " machine and put your real age in.")
+                End Select
+
+            Catch ex As Exception
+                Console.WriteLine($"You entered {userInput}.")
+            End Try
+
+
+        Loop Until userInput = "Quit"
+
+        Do Until userInput = "Quit"
+            Console.WriteLine("Type Quit to End.")
+        Loop
 
     End Sub
 
